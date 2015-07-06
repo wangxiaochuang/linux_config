8ae6f05b885ad1fd5d11e1683ee926a074741b73
wangxiaochuang <wangxiaochuang@paypalm.cn>
2015-07-06 17:41:26 +0800

    auto

--- upload.sh
+++ upload.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-config_file="
-.tmux.conf .vim .vimrc .Xmodmap
-"
-
-for file in $config_file
-do
-    cp -rf ~/$file config
-done
-
-git add config synchronism.sh
+git add config upload.sh
 git commit -m "auto"
 git push -u origin master
