983f12b0163a381e608d8693daf34399315f0b92
wangxiaochuang <suse@linux.site>
2015-11-01 02:52:39 +0800

    update

--- resource/program/versionctrl/version/views.py
+++ resource/program/versionctrl/version/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
+from django.core import serializers
 from django.http import HttpResponse
 from version.models import Gitnote
+import json
 
 # Create your views here.
 def index(request):
-    notes = Gitnote.objects.all()
-    return render(request, 'home.html', {'notes':notes})
+    noteall = Gitnote.objects.all()
+    projs = Gitnote.objects.values('projName').distinct()
+    return render(request, 'home.html', {'noteall':noteall, 'projs':projs})
 
 def add(request):
     return HttpResponse('test')
 
 def filter(request):
     data = request.GET.get('projName', '')
-    res = Gitnote.objects.get(projName=data)
-    return HttpResponse(simplejson.dumps(res), mimetype="application/javascript")
+    res = serializers.serialize("json", Gitnote.objects.filter(projName=data))  
+    return HttpResponse(res, content_type="application/json")
